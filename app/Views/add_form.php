b3670fb3a3471dd01e57f795e0d706918c888c9e
zelmano <zelman.brun@gmail.com>
2024-06-25 10:51:38 +0200

    squelette formulaire

---
+++ app/Views/add_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+  <title>Ajout</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="stylax.css">
+</head>
+<body>
+  <p>Formulaire d'ajout</p>
+</body>
+</html>
