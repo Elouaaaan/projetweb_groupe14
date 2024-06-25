c9f63fbfc4ccaa11cbfd25ad60d73b00c9e9a56f
Elouaaaan <elouan.pennec@gmail.com>
2024-06-25 07:12:30 +0000

    assets

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Tree</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    
 </head>
 
 <body>
-    <h1>Welcome to Tree</h1>
+    <?php 
+    require_once __DIR__ . '/app/Views/header.php'; 
+    echo $header;
+    ?>
 </body>
 
 <?php
