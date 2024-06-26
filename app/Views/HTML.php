da37d0eebe63f7dda3efebca8aaf3bf3e057c3a2
Elouaaaan <elouan.pennec@gmail.com>
2024-06-26 17:40:33 +0200

    marche pas

--- app/Views/HTML.php
+++ app/Views/HTML.php
@@ -24,7 +24,7 @@ class HTML
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-            <title>{getenv('APP_NAME')}</title>
+            <title></title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             {$cssLinks}
             {$jsScripts}
