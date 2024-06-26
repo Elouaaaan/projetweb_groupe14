0080e5c98a97db9f65fa945c43a56144f243a9a0
zelmano <zelman.brun@gmail.com>
2024-06-26 09:22:06 +0200

    tableau & CARTE

---
+++ app/Views/table.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <title>Tableau</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+<body>
+  <header>
+    <h1>SQArbres</h1>
+    <nav>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Ajout</a></li>
+        <li><a href="#">Carte & Tableau</a></li>
+        <li><a href="#">Clusters</a></li>
+        <li><a href="#">Âge et déracinage</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <table>
+    <tr>
+      <th>Longitude</th>
+      <th>Latitude</th>
+      <th>Quartier</th>
+      <th>Secteur</th>
+      <th>Hauteur totale</th>
+      <th>Hauteur tronc</th>
+      <th>Diamètre tronc</th>
+      <th>État arbre</th>
+      <th>Stade de développement</th>
+      <th>Port</th>
+      <th>Pied</th>
+      <th>Situation</th>
+      <th>Revêtement</th>
+      <th>Âge estimé</th>
+      <th>Précision estimée</th>
+      <th>Nombre de diagnostics</th>
+      <th>Nom technique</th>
+      <th>Villeca</th>
+      <th>Feuillage</th>
+      <th>Remarquable</th>
+    </tr>
+    <tr>
+      <td>3.2932636093638927</td>
+      <td>49.84050020512298</td>
+      <td>Quartier du Centre-Ville</td>
+      <td>Quai Gayant</td>
+      <td>6.0</td>
+      <td>2.0</td>
+      <td>37.0</td>
+      <td>EN PLACE</td>
+      <td>Jeune</td>
+      <td>semi libre</td>
+      <td>gazon</td>
+      <td>Alignement</td>
+      <td>Non</td>
+      <td>15.0</td>
+      <td>5.0</td>
+      <td>0.0</td>
+      <td>QUERUB</td>
+      <td>VILLE</td>
+      <td>Feuillu</td>
+      <td>Non</td>
+    </tr>
+    <tr>
+      <td>3.273379794709001</td>
+      <td>49.861408713361236</td>
+      <td>Quartier du Vermandois</td>
+      <td>Stade Cepy</td>
+      <td>13.0</td>
+      <td>1.0</td>
+      <td>160.0</td>
+      <td>EN PLACE</td>
+      <td>Adulte</td>
+      <td>semi libre</td>
+      <td>gazon</td>
+      <td>Groupe</td>
+      <td>Non</td>
+      <td>50.0</td>
+      <td>10.0</td>
+      <td>0.0</td>
+      <td>PINNIGnig</td>
+      <td>VILLE</td>
+      <td>Conifère</td>
+      <td>Non</td>
+    </tr>
+    <tr>
+      <td>3.289067824663547</td>
+      <td>49.84451310660708</td>
+      <td>Quartier du Centre-Ville</td>
+      <td>Rue Villebois Mareuil</td>
+      <td>12.0</td>
+      <td>3.0</td>
+      <td>116.0</td>
+      <td>REMPLACÉ</td>
+      <td>Adulte</td>
+      <td>semi libre</td>
+      <td>gazon</td>
+      <td>Alignement</td>
+      <td>Non</td>
+      <td>30.0</td>
+      <td>10.0</td>
+      <td>0.0</td>
+      <td>ACEPSE</td>
+      <td>VILLE</td>
+      <td>Feuillu</td>
+      <td>Non</td>
+  </table>
+
+  <iframe src="./lll.html" name="carte" title="Carte">
+  </iframe>
+
+
+  <footer>
+    <p>Il est beau mon footer hein ?</p>
+  </footer>
+
+</body>
+</html>
