92a705aef8438b359817aaef0a220dee9d40c0c8
zelmano <zelman.brun@gmail.com>
2024-06-26 16:58:41 +0200

    nouveau format html

--- app/Views/Accueil.php
+++ app/Views/Accueil.php
@@ -10,10 +10,12 @@ class Accueil
     ?>
 
     <main>
-      <h2>Arbres de Saint-Quentin</h2>
-      <h3>02100</h3>
-      <a id="decouvrir" href="#descriptif">Découvrir</a>
-      <img src="assets/images/stquentin.webp" alt="ville de Saint-Quentin">
+      <div class="accueil-img-container">
+        <h2>Arbres de Saint-Quentin</h2>
+        <h3>02100</h3>
+        <a id="decouvrir" href="#descriptif">Découvrir</a>
+        <img src="assets/images/stquentin.webp" alt="ville de Saint-Quentin">
+      </div>
       <h4 id="descriptif">Descriptif rapide</h4>
       <p>lala Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus debitis quae eligendi praesentium quisquam, perspiciatis minus nostrum aliquam maiores veritatis earum et sequi nam laudantium quaerat vero eum laboriosam dolorem.</p>
     </main>
