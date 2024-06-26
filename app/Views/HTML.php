415f0f6b2cd0c97ce99c9c45617736225d24b5fe
zelmano <zelman.brun@gmail.com>
2024-06-26 15:45:20 +0200

    nouveau css main

--- app/Views/HTML.php
+++ app/Views/HTML.php
@@ -14,6 +14,7 @@ class HTML {
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
             <title>' . getenv('APP_NAME') . '</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="assets/css/main.css">
             <link rel="stylesheet" type="text/css" href="assets/css/header.css">
             <link rel="stylesheet" type="text/css" href="assets/css/form.css">
             <link rel="stylesheet" type="text/css" href="assets/css/footer.css">
