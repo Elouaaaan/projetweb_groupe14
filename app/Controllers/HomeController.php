e850e19fd928714d79f582e4600b7a618ee04ac1
zelmano <zelman.brun@gmail.com>
2024-06-27 17:40:22 +0200

    clusters

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -21,6 +21,7 @@ use App\Views\Accueil;
 use App\Views\Form;
 use App\Views\FormRow;
 use App\Views\Tableau;
+use App\Views\Clusters;
 
 class HomeController
 {
@@ -169,5 +170,20 @@ class HomeController
         ];
 
         echo HTML::generateHTML($header, $content, $footer, $cssFiles, $jsFiles);
+    }
+
+    static public function clusters()
+    {
+        $header = (new Header())->render();
+        $content = "<div id='clusters'>Les clusters</div>";
+        $footer = (new Footer())->render();
+
+        $cssFiles = [
+            'main.css',
+            'header.css',
+            'footer.css',
+        ];
+
+        echo HTML::generateHTML($header, $content, $footer, $cssFiles);
     }
 }
