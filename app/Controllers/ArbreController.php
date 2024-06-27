dd4a4489298e40c29d09ae9b14ebb9e4a7b36685
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 19:25:28 +0200

    hehe

--- app/Controllers/ArbreController.php
+++ app/Controllers/ArbreController.php
@@ -76,9 +76,7 @@ class ArbreController
         $json_data = tempnam(sys_get_temp_dir(), 'json_data');
         file_put_contents($json_data, json_encode($result));
 
-        $output = shell_exec(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/cluster_models.py ' . $json_data);
-
-        echo json_encode(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/cluster_models.py ' . $json_data);
+        $output = shell_exec(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/cluster.py ' . $json_data . ' ' . '1');
 
         unlink($json_data);
 
--- python/cluster.py
+++ python/cluster.py
@@ -5,33 +5,22 @@ import pandas as pd
 import numpy as np
 import joblib
 
-df = pd.read_json(sys.argv[1])
-    
-    df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
-    
-    preprocessor_filename = os.path.join(dirname, 'models/preprocessor.pkl')
-    preprocessor = joblib.load(preprocessor_filename)
+dirname = os.path.dirname(os.path.abspath(__file__))
 
-    X = preprocessor.transform(df)
+df = pd.read_json(sys.argv[1])
+df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
     
-    kmeans_2_clusters_filename = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
-    kmeans_3_clusters_filename = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
-    db_filename = os.path.join(dirname, 'models/dbscan.pkl')
-    
-    models = {
-        'kmeans_2_clusters': joblib.load(kmeans_2_clusters_filename),
-        'kmeans_3_clusters': joblib.load(kmeans_3_clusters_filename),
-        'db': joblib.load(db_filename)
-    }
-    
-    match sys.argv[2]:
-        case '1':
-            y_pred = models['kmeans_2_clusters'].predict(X)
-        case '2':
-            y_pred = models['kmeans_3_clusters'].predict(X)
-        case '3':
-            y_pred = models['db'].fit_predict(X)
+match sys.argv[2]:
+    case '1':
+        model_path = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
+    case '2':
+        model_path = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
+    case '3':
+        model_path = os.path.join(dirname, 'models/dbscan.pkl')
+
+X = joblib.load(os.path.join(dirname, 'models/preprocessor.pkl')).transform(df)
+y_pred = joblib.load(model_path).predict(X)
             
-    df['cluster'] = y_pred
-    result = df.to_json(orient='records')
-    print(result)
+df['cluster'] = y_pred
+result = df.to_json(orient='records')
+print(result)
