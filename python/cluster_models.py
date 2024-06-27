c6941b8c3f0817065655566ccecbe175406b8b19
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 17:15:40 +0200

    hehehe

--- python/cluster_models.py
+++ python/cluster_models.py
@@ -20,52 +20,44 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
 mycursor.execute("SELECT haut_tot, tronc_diam, port FROM arbre JOIN port USING(id_port)")
-
 df = pd.DataFrame(mycursor.fetchall(), columns=['haut_tot', 'tronc_diam', 'port'])
 
+df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
+features = ['haut_tot', 'tronc_section', 'port']
+X = df[features]
 
-print(df)
+scaler = StandardScaler()
+num_features = ['haut_tot', 'tronc_section']
+encoder = OneHotEncoder()
+cat_features = ['port']
 
-# Connect to the database
-# $PLACEHOLDER$
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', scaler, num_features),
+        ('cat', encoder, cat_features)
+    ],
+    sparse_threshold=0,
+    remainder='passthrough'
+)
 
-# df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
-# features = ['haut_tot', 'tronc_section', 'port']
-# X = df[features]
+# Fit and transform the data using the preprocessor
+X_prep = preprocessor.fit_transform(X)
 
-# scaler = StandardScaler()
-# num_features = ['haut_tot', 'tronc_section']
-# encoder = OneHotEncoder()
-# cat_features = ['port']
+# Train the KMeans model with 2 clusters
+kmeans_2_clusters = KMeans(n_clusters=2)
+kmeans_2_clusters.fit(X_prep)
 
-# preprocessor = ColumnTransformer(
-#     transformers=[
-#         ('num', scaler, num_features),
-#         ('cat', encoder, cat_features)
-#     ],
-#     sparse_threshold=0,
-#     remainder='passthrough'
-# )
+# Train the KMeans model with 3 clusters
+kmeans_3_clusters = KMeans(n_clusters=3)
+kmeans_3_clusters.fit(X_prep)
 
-# # Fit and transform the data using the preprocessor
-# X_prep = preprocessor.fit_transform(X)
+# Train the DBSCAN model
+dbscan = DBSCAN(eps=0.65, min_samples=3)
+dbscan.fit(X_prep)
 
-# # Train the KMeans model with 2 clusters
-# kmeans_2_clusters = KMeans(n_clusters=2)
-# kmeans_2_clusters.fit(X_prep)
-
-# # Train the KMeans model with 3 clusters
-# kmeans_3_clusters = KMeans(n_clusters=3)
-# kmeans_3_clusters.fit(X_prep)
-
-# # Train the DBSCAN model
-# dbscan = DBSCAN(eps=0.65, min_samples=3)
-# dbscan.fit(X_prep)
-
-# # Save the models and preprocessor
-# joblib.dump(preprocessor, 'models/preprocessor.pkl')
-# joblib.dump(kmeans_2_clusters, 'models/kmeans_2_clusters.pkl')
-# joblib.dump(kmeans_3_clusters, 'models/kmeans_3_clusters.pkl')
-# joblib.dump(dbscan, 'models/dbscan.pkl')
+# Save the models and preprocessor
+joblib.dump(preprocessor, 'models/preprocessor.pkl')
+joblib.dump(kmeans_2_clusters, 'models/kmeans_2_clusters.pkl')
+joblib.dump(kmeans_3_clusters, 'models/kmeans_3_clusters.pkl')
+joblib.dump(dbscan, 'models/dbscan.pkl')
