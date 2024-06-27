acdf62863c946a6dd9be03eb65ca80314b62a7bc
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 20:15:37 +0200

    aaaaa

--- assets/js/clusters.js
+++ assets/js/clusters.js
@@ -8,7 +8,7 @@ fetch('api/request.php/clusters/?cluster=1', {
         return response.json();
     })
     .then(data => {
-        console.log(typeof data);
+        console.log(data);
     })
     .catch(error => {
         console.error('Error:', error);
