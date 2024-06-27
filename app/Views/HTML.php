98bf4c675f3412564f5f04a6211d3428adce1053
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 10:11:56 +0200

    marche

--- app/Views/HTML.php
+++ app/Views/HTML.php
@@ -45,8 +45,6 @@ class HTML
 
     public static function generateHTML($header, $content, $footer, $cssFiles = [], $jsFiles = [])
     {
-        echo $content;
-        echo $footer;
         return '
         <!DOCTYPE html>
         <html lang="fr">
