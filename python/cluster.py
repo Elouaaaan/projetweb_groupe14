bc197c0852c11e986d9aa2198a0345699e570588
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 22:32:48 +0200

    aaa

--- python/cluster.py
+++ python/cluster.py
@@ -19,22 +19,27 @@ df = pd.read_json(json_string)
 
 df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
 
-model_path = None
 model_choice = sys.argv[2]
-
-if model_choice == '1':
-    model_path = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
-elif model_choice == '2':
-    model_path = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
-elif model_choice == '3':
-    model_path = os.path.join(dirname, 'models/dbscan.pkl')
-else:
-    raise ValueError("Invalid model choice. Please choose from '1', '2', or '3'.")
 
 preprocessor = joblib.load(os.path.join(dirname, 'models/preprocessor.pkl'))
 X = preprocessor.transform(df[['haut_tot', 'tronc_section', 'port']])
-model = joblib.load(model_path)
-y_pred = model.predict(X)
+
+
+if model_choice == '1':
+    model_path = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
+    model = joblib.load(model_path)
+    y_pred = model.predict(X)
+elif model_choice == '2':
+    model_path = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
+    model = joblib.load(model_path)
+    y_pred = model.predict(X)
+elif model_choice == '3':
+    model_path = os.path.join(dirname, 'models/dbscan.pkl')
+    model = joblib.load(model_path)
+    model.fit(X)
+    y_pred = model.labels_
+else:
+    raise ValueError("Invalid model choice. Please choose from '1', '2', or '3'.")
 
 df['cluster'] = y_pred
 result = df.to_json(orient='records')
