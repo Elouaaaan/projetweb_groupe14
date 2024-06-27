c5abed89811c1a36b3791ccb783422123af74122
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 22:11:20 +0200

    ===

--- app/Views/Clusters.php
+++ app/Views/Clusters.php
@@ -13,7 +13,7 @@ class Clusters
               <div class="radio-group">
                 <input type="radio" id="deux" name="choix-clusters" value="1" checked>
                 <label for="deux">Deux clusters</label>
-                <input type="radio" id="trois" name="choix-clusters" value"2">
+                <input type="radio" id="trois" name="choix-clusters" value="2">
                 <label for="trois">Trois clusters</label>
                 <input type="radio" id="anomalies" name="choix-clusters" value="3">
                 <label for="anomalies">Détection des anomalies</label>
