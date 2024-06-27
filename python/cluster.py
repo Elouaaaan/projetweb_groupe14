a522efa6f06045444297c72955bab7d0b26c3d7b
Elouaaaan <elouan.pennec@gmail.com>
2024-06-27 19:28:48 +0200

    hehe

--- python/cluster.py
+++ python/cluster.py
@@ -10,18 +10,26 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 
 df = pd.read_json(sys.argv[1])
 df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
-    
-match sys.argv[2]:
-    case '1':
-        model_path = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
-    case '2':
-        model_path = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
-    case '3':
-        model_path = os.path.join(dirname, 'models/dbscan.pkl')
 
-X = joblib.load(os.path.join(dirname, 'models/preprocessor.pkl')).transform(df)
-y_pred = joblib.load(model_path).predict(X)
-            
+# Handling different model choices based on sys.argv[2]
+model_path = None
+model_choice = sys.argv[2]
+
+if model_choice == '1':
+    model_path = os.path.join(dirname, 'models/kmeans_2_clusters.pkl')
+elif model_choice == '2':
+    model_path = os.path.join(dirname, 'models/kmeans_3_clusters.pkl')
+elif model_choice == '3':
+    model_path = os.path.join(dirname, 'models/dbscan.pkl')
+else:
+    raise ValueError("Invalid model choice. Please choose from '1', '2', or '3'.")
+
+# Load preprocessor and predict clusters
+preprocessor = joblib.load(os.path.join(dirname, 'models/preprocessor.pkl'))
+X = preprocessor.transform(df)
+model = joblib.load(model_path)
+y_pred = model.predict(X)
+
 df['cluster'] = y_pred
 result = df.to_json(orient='records')
 print(result)
