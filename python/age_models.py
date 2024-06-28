92a3b09df13ed2fc9f005e1fd657e00470287e42
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 10:54:58 +0200

    aaa

--- python/age_models.py
+++ python/age_models.py
@@ -1,12 +1,7 @@
-import math
 import os
-from typing import Self, Optional
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import joblib
-from joblib import Parallel, delayed
 
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error
