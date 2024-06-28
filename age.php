4bac9306e88c8b0b7104f72ad84c203eda4ee21b
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 10:18:34 +0200

    test

--- age.php
+++ age.php
@@ -4,9 +4,4 @@ require __DIR__ . '/bootstrap.php';
 
 use App\Controllers\HomeController;
 
-if (empty($_GET['id_arbre'])) {
-    header('Location: /tableaucarte.php');
-    exit;
-}
-
 echo HomeController::age();
