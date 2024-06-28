7e68037cd15a5fca48551beed56827494f5eced6
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 11:50:30 +0200

    AAAA

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -214,7 +214,6 @@ class HomeController
         file_put_contents($json_data, json_encode($result, JSON_UNESCAPED_UNICODE));
         $output = shell_exec(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/age.py ' . $json_data);
         unlink($json_data);
-        $output = json_decode($output, true);
 
         var_dump($output);
 
