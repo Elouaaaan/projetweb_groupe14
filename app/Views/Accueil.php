c681667fe40fa8787bbbb8582596ba93dac7caa1
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 12:57:03 +0200

    fini la

--- app/Views/Accueil.php
+++ app/Views/Accueil.php
@@ -7,20 +7,19 @@ class Accueil
   public function render()
   {
     ob_start();
-    ?>
+?>
 
     <main>
       <div class="accueil-img-container">
         <h2>Arbres de Saint-Quentin</h2>
         <h3>02100</h3>
-        <a id="decouvrir" href="#descriptif">Découvrir</a>
         <img src="assets/images/stquentin.webp" alt="ville de Saint-Quentin">
       </div>
       <h4 id="descriptif">Descriptif rapide</h4>
       <p>lala Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus debitis quae eligendi praesentium quisquam, perspiciatis minus nostrum aliquam maiores veritatis earum et sequi nam laudantium quaerat vero eum laboriosam dolorem.</p>
     </main>
 
-    <?php
+<?php
 
 
     return ob_get_clean();
