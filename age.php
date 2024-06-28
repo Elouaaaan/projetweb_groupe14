fd4baf868dee677dea1223841c836906d2da4d7a
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 10:14:22 +0200

    azaz

--- age.php
+++ age.php
@@ -4,8 +4,6 @@ require __DIR__ . '/bootstrap.php';
 
 use App\Controllers\HomeController;
 
-var_dump($_GET);
-
 if (empty($_GET['id_arbre'])) {
     header('Location: /tableaucarte.php');
     exit;
