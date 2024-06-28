27f82b7204c19323a115f9292eea44043f66b962
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 12:17:08 +0200

    aazeazeazeaze

--- app/Views/Age.php
+++ app/Views/Age.php
@@ -26,14 +26,23 @@ class Age
 
   public function render()
   {
+    $sum = 0;
+    $count = count($this->age_info);
+
+    foreach ($this->age_info as $age) {
+      $sum += $age['age'];
+    }
+    $mean = $sum / $count;
+
+
     ob_start();
 ?>
 
     <main>
 
       <div class="head">
-        <p class="resultat-final"></p>
-        <p class="moyenne">Moyenne d'âge prédit par <?php echo count($this->age_info); ?> modèles</p>
+        <p class="resultat-final"><?php echo $mean; ?></p>
+        <p class="moyenne">Moyenne d'âge prédit par <?php echo $count; ?> modèles</p>
       </div>
       </div>
 
@@ -43,38 +52,37 @@ class Age
             <p class="nom_modele">Modèle <?php echo $age['modele']; ?></p>
             <p class="resultat"><?php echo number_format($age['age'], 2); ?> ans</p>
           </div>
+        <?php endforeach; ?>
       </div>
-    <?php endforeach; ?>
-    </div>
 
 
-    <div class="conteneur-boites">
-      <div class="boite">
-        <p class="nom_modele">modèle 1</p>
-        <p class="resultat">5,92 ans</p>
-        <p class="commentaire">temps d'éxecution: 0,24s</p>
+      <div class="conteneur-boites">
+        <div class="boite">
+          <p class="nom_modele">modèle 1</p>
+          <p class="resultat">5,92 ans</p>
+          <p class="commentaire">temps d'éxecution: 0,24s</p>
+        </div>
+        <div class="boite">
+          <p class="nom_modele">modèle 2</p>
+          <p class="resultat">6,38 ans</p>
+          <p class="commentaire">temps d'éxecution: 0,32s</p>
+        </div>
+        <div class="boite">
+          <p class="nom_modele">modèle 3</p>
+          <p class="resultat">6,12 ans</p>
+          <p class="commentaire">temps d'éxecution: 0,28s</p>
+        </div>
+        <div class="boite">
+          <p class="nom_modele">modèle 4</p>
+          <p class="resultat">5,58 ans</p>
+          <p class="commentaire">temps d'éxecution: 0,26s</p>
+        </div>
       </div>
-      <div class="boite">
-        <p class="nom_modele">modèle 2</p>
-        <p class="resultat">6,38 ans</p>
-        <p class="commentaire">temps d'éxecution: 0,32s</p>
-      </div>
-      <div class="boite">
-        <p class="nom_modele">modèle 3</p>
-        <p class="resultat">6,12 ans</p>
-        <p class="commentaire">temps d'éxecution: 0,28s</p>
-      </div>
-      <div class="boite">
-        <p class="nom_modele">modèle 4</p>
-        <p class="resultat">5,58 ans</p>
-        <p class="commentaire">temps d'éxecution: 0,26s</p>
-      </div>
-    </div>
 
-    <div class="head">
-      <p class="resultat-final">Risque de déracinement il y a 1 mois</p>
-      <p class="moyenne"><?php echo number_format($this->risque, 2); ?>%</p>
-    </div>
+      <div class="head">
+        <p class="resultat-final">Risque de déracinement il y a 1 mois</p>
+        <p class="moyenne"><?php echo number_format($this->risque, 2); ?>%</p>
+      </div>
 
     </main>
 
