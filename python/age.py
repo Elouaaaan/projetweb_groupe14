6e3b7443e158538d43103efbe863b7bf7aebd1d9
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 11:18:50 +0200

    test

--- python/age.py
+++ python/age.py
@@ -33,18 +33,24 @@ encoded_categorical = categorical_encoder.transform(df[categorical_features])
 encoded_categorial_df = pd.DataFrame(encoded_categorical, columns=categorical_encoder.get_feature_names_out(categorical_features))
 
 models = {
-    'ridge_model': 'models/ridge.pkl',
+    'ridge_model': 'models/age_ridge.pkl',
+    'svr_linear': 'models/age_svr_linear.pkl',
+    'svr_rbf': 'models/age_svr_rbf.pkl',
+    'random_forest': 'models/random_forest.pkl
 }
 
-model_filename = os.path.join(dirname, 'models/random_forest.pkl')
-model = joblib.load(model_filename)
 
 X = pd.concat([scaled_numerical_df, encoded_categorial_df], axis=1)
 X = X[model.feature_names_in_]
-    
-y_pred = model.predict(X)
 
-df['age'] = y_pred
+
+for model_name, model_filename in models.items():
+    model = joblib.load(model_filename)
+    
+    y_pred = model.predict(X)
+    
+    df[model_name] = y_pred
+    
 
 result = df.to_json(orient='records')
 print(result)
