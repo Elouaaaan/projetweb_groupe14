992eccaac7cabef6d5a85c2fd9805cb7534ccdcf
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 11:23:20 +0200

    pray

--- python/age.py
+++ python/age.py
@@ -22,12 +22,12 @@ df['tronc_section'] = np.pi * (df['tronc_diam'] / 2) ** 2
 numerical_features = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
 categorical_features = df.select_dtypes(include=['object']).columns.tolist()
         
-scaler_filename = os.path.join(dirname, 'models/scaler.pkl')
+scaler_filename = os.path.join(dirname, 'models/age_scaler.pkl')
 scaler = joblib.load(scaler_filename)
 scaled_numerical = scaler.transform(df[numerical_features])
 scaled_numerical_df = pd.DataFrame(scaled_numerical, columns=numerical_features)
     
-categorical_encoder_filename = os.path.join(dirname, 'models/onehot_encoder.pkl')
+categorical_encoder_filename = os.path.join(dirname, 'models/age_onehot_encoder.pkl')
 categorical_encoder = joblib.load(categorical_encoder_filename)
 encoded_categorical = categorical_encoder.transform(df[categorical_features])
 encoded_categorial_df = pd.DataFrame(encoded_categorical, columns=categorical_encoder.get_feature_names_out(categorical_features))
