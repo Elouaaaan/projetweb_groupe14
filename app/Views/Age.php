78f386365f376e119c3c7e6f4cb4702c888d1307
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 12:18:01 +0200

    format

--- app/Views/Age.php
+++ app/Views/Age.php
@@ -55,30 +55,6 @@ class Age
         <?php endforeach; ?>
       </div>
 
-
-      <div class="conteneur-boites">
-        <div class="boite">
-          <p class="nom_modele">modèle 1</p>
-          <p class="resultat">5,92 ans</p>
-          <p class="commentaire">temps d'éxecution: 0,24s</p>
-        </div>
-        <div class="boite">
-          <p class="nom_modele">modèle 2</p>
-          <p class="resultat">6,38 ans</p>
-          <p class="commentaire">temps d'éxecution: 0,32s</p>
-        </div>
-        <div class="boite">
-          <p class="nom_modele">modèle 3</p>
-          <p class="resultat">6,12 ans</p>
-          <p class="commentaire">temps d'éxecution: 0,28s</p>
-        </div>
-        <div class="boite">
-          <p class="nom_modele">modèle 4</p>
-          <p class="resultat">5,58 ans</p>
-          <p class="commentaire">temps d'éxecution: 0,26s</p>
-        </div>
-      </div>
-
       <div class="head">
         <p class="resultat-final">Risque de déracinement il y a 1 mois</p>
         <p class="moyenne"><?php echo number_format($this->risque, 2); ?>%</p>
