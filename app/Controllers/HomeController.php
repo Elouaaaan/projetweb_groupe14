812e82b29b0904950553988c2e372f72b23b1ea0
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 10:23:55 +0200

    aeae

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -205,12 +205,11 @@ class HomeController
         $json_data = tempnam(sys_get_temp_dir(), 'json_data');
         file_put_contents($json_data, json_encode($result, JSON_UNESCAPED_UNICODE));
 
-        $output = shell_exec(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/tempete.py ' . $json_data . ' 2>&1');
+        $output = shell_exec(__DIR__ . '/../../venv/bin/python3 ' . __DIR__ . '/../../python/tempete.py ' . $json_data);
 
         unlink($json_data);
 
-
-        echo $output;
+        echo $output['proba_deracinage'];
 
         $header = (new Header())->render();
         $content = (new Age())
