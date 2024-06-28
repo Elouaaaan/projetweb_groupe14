a2fafbef3ba4016fe36087344d5a47cf17c4b840
Elouaaaan <elouan.pennec@gmail.com>
2024-06-28 09:58:06 +0200

    header

--- age.php
+++ age.php
@@ -5,6 +5,11 @@ require __DIR__ . '/bootstrap.php';
 use App\Controllers\HomeController;
 use App\Controllers\ArbreController;
 
+if (empty($_GET['age'])) {
+    header('Location: /tableaucarte.php');
+    exit;
+}
+
 echo (new ArbreController('GET'))->getUprooted(1);
 
 // echo HomeController::age();
